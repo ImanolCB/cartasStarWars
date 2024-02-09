2c6cfc072419b4f193f1fbe96708d02289efa9dd
daw207 <icallejob01@educantabria.es>
2024-02-09 04:45:23 +0100

    Creación de elementos de cartas con DOM y funcion de consulta de nombre

--- js/main.js
+++ js/main.js
@@ -1,2 +1,26 @@
 'use strict'
 
+import {getNombrePj} from './personaje.js';
+
+const contenedorCartas = document.createElement("div");
+const div = document.createElement("div");
+div.className = "contenedor"
+
+for (let i = 1; i <= 6; i++) {
+    let enlace = document.createElement("a");
+    let tituloCarta = document.createElement("h3");
+    let imagenCarta = document.createElement("img");
+    
+    enlace.src= `https://swapi.dev/api/people/${i}`;
+    enlace.id = i;
+    tituloCarta.textContent = getNombrePj(i);
+    imagenCarta.src = `../img/${i}.jpeg`;
+    
+    enlace.appendChild(tituloCarta);
+    enlace.appendChild(imagenCarta);
+    div.appendChild(enlace);
+}
+
+document.body.appendChild(div);
+
+
--- js/personaje.js
+++ js/personaje.js
@@ -1,6 +1,4 @@
 'use strict'
-var diaRegistro;
-const day = date.getDate();
 
 /**
  * ? Página para consultar los personajes https://swapi.dev/api/films --> Como opción añadir al value de cada card la url de cada PJ
@@ -13,15 +11,16 @@ const day = date.getDate();
  * ? Página para consultar la información de - Beru Whitesun lars -     https://swapi.dev/api/people/7/ 
  * ? Página para consultar la información de - R5-D4 -                  https://swapi.dev/api/people/8/ 
  * ? Página para consultar la información de - Biggs Darklighter -      https://swapi.dev/api/people/9/ 
- * ? Página para consultar la información de - Obi-Wan Kenobi -         https://swapi.dev/api/people/10/ 
- * ? Página para consultar la información de - Wilhuff Tarkin -         https://swapi.dev/api/people/12/ 
- * ? Página para consultar la información de - Chewbacca -              https://swapi.dev/api/people/13/ 
- * ? Página para consultar la información de - Han Solo -               https://swapi.dev/api/people/14/ 
- * ? Página para consultar la información de - Greedo -                 https://swapi.dev/api/people/15/ 
- * ? Página para consultar la información de - Jabba Desilijic Tiure -  https://swapi.dev/api/people/16/ 
- * ? Página para consultar la información de - Wedge Antilles -         https://swapi.dev/api/people/18/ 
- * ? Página para consultar la información de - Jek Tono Porkins -       https://swapi.dev/api/people/19/ 
- * ? Página para consultar la información de - Raymus Antilles -        https://swapi.dev/api/people/81/ 
+ * ? Página para consultar la información de - Obi-Wan Kenobi -         https://swapi.dev/api/people/10/
+ *  
+ *  Página para consultar la información de - Wilhuff Tarkin -         https://swapi.dev/api/people/12/ 
+ *  Página para consultar la información de - Chewbacca -              https://swapi.dev/api/people/13/ 
+ *  Página para consultar la información de - Han Solo -               https://swapi.dev/api/people/14/ 
+ *  Página para consultar la información de - Greedo -                 https://swapi.dev/api/people/15/ 
+ *  Página para consultar la información de - Jabba Desilijic Tiure -  https://swapi.dev/api/people/16/ 
+ *  Página para consultar la información de - Wedge Antilles -         https://swapi.dev/api/people/18/ 
+ *  Página para consultar la información de - Jek Tono Porkins -       https://swapi.dev/api/people/19/ 
+ *  Página para consultar la información de - Raymus Antilles -        https://swapi.dev/api/people/81/ 
  * 
  * TODO: Planeta se obtiene de la consulta a otro enlace (API - Fetch)
  */
@@ -29,7 +28,7 @@ const day = date.getDate();
 const listaPersonajes = [];
 
 //Creacion de un objeto del personaje que se cargará con los valores obtenidos de JSON
-function crearPersonaje(nombre,altura,masa,colorPelo,colorPiel,colorOjo,nacimiento,genero,naves,creado,editado){
+function cargarPersonaje(nombre, altura, masa, colorPelo, colorPiel, colorOjo, nacimiento, genero, naves, creado, editado) {
     var pj = {
         nombre: nombre,
         altura: altura,
@@ -40,16 +39,24 @@ function crearPersonaje(nombre,altura,masa,colorPelo,colorPiel,colorOjo,nacimien
         nacimiento: nacimiento,
         genero: genero,
         planeta: getPlaneta(),
-        peliculas: getPeliculas(), 
-        especies: getEspecies(), 
-        vehiculos: vehiculos, 
-        naves: naves, 
-        creado: creado, 
-        editado: editado, 
+        peliculas: getPeliculas(),
+        especies: getEspecies(),
+        vehiculos: vehiculos,
+        naves: naves,
+        creado: creado,
+        editado: editado,
     }
     listaPersonajes.push(pj);
 }
 
-function getPlaneta() {
-    
+function getNombrePj(pj) {
+
+    $.get(`https://swapi.dev/api/people/${pj}`, function (data, status) {
+        let nombre = data.height;
+        return nombre;
+    });
 }
+
+
+
+export {getNombrePj};
