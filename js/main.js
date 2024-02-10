1f77f1a6b3766d46650523f5f5c31cf537ac843a
daw207 <icallejob01@educantabria.es>
2024-02-10 18:00:56 +0100

    Modificacion de imagenes y eliminación de enlaces json de los comentarios

--- js/main.js
+++ js/main.js
@@ -2,17 +2,19 @@
 
 import {getNombrePj} from './personaje.js';
 
+const nombres = []
 const contenedorCartas = document.createElement("div");
 const div = document.createElement("div");
 div.className = "contenedor"
 
-for (let i = 1; i <= 6; i++) {
+for (let i = 1; i <= 5; i++) {
     let enlace = document.createElement("a");
     let tituloCarta = document.createElement("h3");
     let imagenCarta = document.createElement("img");
     
-    enlace.src= `https://swapi.dev/api/people/${i}`;
+    enlace.href= `https://swapi.dev/api/people/${i}`;
     enlace.id = i;
+    console.log(getNombrePj(i));
     tituloCarta.textContent = getNombrePj(i);
     imagenCarta.src = `../img/${i}.jpeg`;
     
