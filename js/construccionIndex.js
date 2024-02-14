38848ff15a8a5157e291c6f7286ab0a3ce02646d
daw207 <icallejob01@educantabria.es>
2024-02-14 03:36:15 +0100

    Adaptación de 'construccionIndex.js' para que se realice con JQuery. DOM -> JQuery

--- js/construccionIndex.js
+++ js/construccionIndex.js
@@ -4,28 +4,28 @@ $(document).ready(function () {
   //Array que almacena los nombres de los personajes
   const nombres = ["Chewbacca", "Darth Vader", "R2-D2", "C-3PO", "Yoda"];
 
-  //Creación de div que contiene las cartas
-  const div = document.createElement("div");
-  div.className = "contenedor";
+  //Creación de div que contiene las cartas con la clase contenedor
+  const div = $("<div>").addClass("contenedor");
+
 
   for (let i = 1; i <= nombres.length; i++) {
     //Creación de etiquetas
-    let enlace = document.createElement("a");
-    let tituloCarta = document.createElement("h3");
-    let imagenCarta = document.createElement("img");
+    let enlace = $("<a>");
+    let tituloCarta = $("<h3>");
+    let imagenCarta = $("<img>");
 
     //Asignación de datos a qcada etiqueta
-    enlace.id = i;
-    tituloCarta.textContent = nombres[i - 1];
-    imagenCarta.src = `../img/${i}.jpeg`;
+    enlace.attr("id", i);
+    tituloCarta.text(nombres[i - 1]);
+    imagenCarta.attr("src", `../img/${i}.jpeg`);
 
     //Estructura de elementos creados
-    enlace.appendChild(tituloCarta);
-    enlace.appendChild(imagenCarta);
-    div.appendChild(enlace);
+    enlace.append(tituloCarta);
+    enlace.append(imagenCarta);
+    div.append(enlace);
   }
   //Aplicación de la estructura creada al body del index
-  document.body.appendChild(div);
+  $("body").append(div);
 
   //Funcion on click que detecta la carta sobre la que se hace click y recoge el valor de texto almacenado en la carta para consultar
   $("a").click(function () {
